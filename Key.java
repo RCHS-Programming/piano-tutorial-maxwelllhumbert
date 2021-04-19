2fa1fee583656876092d99050cfa6742980603e9
maxwelllhumbert <maxwell.humbert@stu.boone.kyschools.us>
2021-04-19 12:09:47 -0400

    v

--- Key.java
+++ Key.java
@@ -2,6 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
+    
     /**
      * Create a new key.
      */
@@ -14,5 +16,21 @@ public class Key extends Actor
      */
     public void act()
     {
+        
+        if ( !isDown && Greenfoot.isKeyDown("g"))
+        {
+            setImage("white-key-down.png");
+            isDown=true;
+        }
+        if ( isDown && !Greenfoot.isKeyDown("g"))
+        {
+            setImage("white-key.png");
+            isDown=false;
+        }
     }
+    public void play()
+    {
+        Greenfoot.playSound ("3a.wav");
+    }
+    
 }
